713750d1b629094790feccfa6b1170ce0943ffcc
bladerunner <bladerunner@new-host-2.home>
2014-03-15 14:51:09 -0400

    Added graph handling

--- src/core.js
+++ src/core.js
@@ -1,88 +1,41 @@
-//Design overview 
-//There are two enitites
-//Core calc process
-//Accounts
-//Core calc process are like Sum,Mean etc
-//Accounts are what a are created as a part of the model which is been
-//There is a core convertor part which joins these two parts to create
-//the computing trees
+//This is the main computation part 
+//For each part of the coreModel, compute the formulas by giving it
+
+var mergeNode = function(template,companyGraph) { 
+   Object.getOwnPropertyNames(template).forEach(val,idx,array) {
+       companyGraph[val] = template[val];
+   }
+}
+
+var mergePath = function(template,companyGraph) {
+    Object.getOwnPropertyName(template).forEach(val,idx,array) {
+        var myNode = template[val]; 
+        if(myNode.hasOwnProperty("_parentNode")) {
+            var parent = find(myNode,companyGraph);
+            parent._formula = current(myNode._parentNode);
+        }
+    }
+}
+
+var mergeGraphs = function(template,companyGraph) { 
+    mergeNode(template.nodes,companyGraph.nodes); 
+    mergePath(template.graph,companyGraph.graph); 
+    return companyGraph;
+}
 
 
-var timeSeries = {
-    LTM : {
-        _function : new function(node) {
-            return function() {
-                sum(filter( range(node,"time"), " { time : > 12 } "));
+var computeGraph = function(nodes,edges,state,engine) {
+    Object.getOwnProperty(edges.data).forEach(val,idx,array) {
+        var edge  = edge.data[val]; 
+        if(state.find(val)) {
+
+        }else {
+            if(edge.hasOwnProperty("_formula")) {
+                var result = engine.compute(edge._formula); 
+                state.add(result,val); 
             }
         }
     }
 }
 
-function Node(name,time,model,scenario,company) {
-    this.name = name;
-    this.time = time;
-    this.model = model;
-    this.scenario = scenario;
-    this.company = company;
-}
 
-
-//This is a truly a acyclic directed graph and should be considered as such 
-//Will store this as an adjaceny list because of the sparseness of the
-//graph . 
-//Notes to self : In a graph 
-//Nodes : are accounts and maybe called vertices 
-//Edges : are directed from owner to ownee 
-//How the graph is structred
-//Model
-//  |-Basic graph
-//    |--Company Specific
-var template = {
-    nodes : {
-        balanceSheet : { },
-        incomeStatement : { },
-        cashFlow : { },
-        asset : {},
-        liability : {},
-        ownerEquity : { },
-    },
-    
-    graph: {
-        actual : {
-            info : {
-                
-            },
-            data : {
-                balanceSheet : {
-                    _formula : sum(asset,liability,ownerEquity),
-                    _neighbour : [asset,liability,ownerEquity]
-                },
-                incomeStatement : {
-                    
-                },
-            }
-        },
-        financial : {
-            
-        }
-    }
-}
-
-
-companyGraph =  {
-    name : "my company",
-    parentGraph : template.graph.actual,
-    nodes : {
-        myLongTermAsset : {},
-        myShortTermAsset : {}
-    },
-    graph : {
-        myLongTermAsset : {
-         _formula : getTemplate("",worksheet,row,column),
-         _parentNode : asset,//This is special case since the
-            //predecessor will be updated at the runtime so this needs
-            //to have an information about it 
-         _neighbour : []
-     }
-    }
-}
