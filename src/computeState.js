de0d9c38b54e1f265b3b2382ab089021f654b6f2
bladerunner <bladerunner@new-host.home>
2014-03-23 12:37:30 -0400

    Added Node creation logic

--- src/computeState.js
+++ src/computeState.js
@@ -26,18 +26,31 @@ function ComputeState(name) {
    }
 
 
+//The node is an important construct.All relationships in real world
+//are between accounts i.e. Asset = Long Term Asset + Short Term
+//Asset.But within the system at computation there are 6 dimensions
+//that uniquely define an account.
+ComputeState.prototype.createNode = 
+function( mname,mdate,mmodel,mscenario,mcompany,mbalance ) { return { Name : mname,  ReportDate : mdate, Model : mmodel,Scenario : mscenario, Company : mcompany, Balance : mbalance } }
+
 //exports.StateManager = ComputeState;
 
-var testComputeState = function() {
+var test = function() {
     var state  = new ComputeState("Company1"); 
-    var o4 = create("oe",-4,"03/31/2013","General");
-    var o3 = create("oe",-3.33,"12/31/2013","General");
-    var o2 = create("liability",20.0,"12/31/2013","General"); 
-    var o1 = create("asset",23.33,"12/31/2013","General");  
-    state.add(o4);
-    state.add(o3);
-    state.add(o2);
-    state.add(o1); 
-     var result = state.find({name:"liability"});
+    print( state.createNode("oe","03/31/2013","General","Actual",state.Name,4.0));
+    var datapt =
+    [
+         state.createNode("oe","03/31/2013","General","Actual",state.Name,4.0)
+        , state.createNode("liability","03/31/2013","General","Actual",state.Name,21.0)
+        , state.createNode("asset","03/31/2013","General","Actual",state.Name,25.0)
+        , state.createNode("oe","12/31/2013","General","Actual",state.Name,10.5)
+        , state.createNode("liability","12/31/2013","General","Actual",state.Name,20.5)
+        , state.createNode("asset","12/31/2013","General","Actual",state.Name,31.0)
+    ];
+    state.add(datapt[0]);
+    state.add(datapt[1]);
+    state.add(datapt[2]);
+    
+    var result = state.find({Name:"liability"});
     return  { State : state, Result : result }; 
 }
