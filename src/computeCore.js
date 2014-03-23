a688f88d64b7f9fdcdd6da98528fccc24e87e5f8
bladerunner <bladerunner@new-host.home>
2014-03-23 14:39:56 -0400

    Fixed computing functions and current

--- src/computeCore.js
+++ src/computeCore.js
@@ -3,39 +3,56 @@
 //needs to be passed to the Core Engine.This should have a single
 //methods called get. Which takes a JSON filter similar to Mongo's Get
 //query where a JSON object represents the query 
-function ComputeCore(dataLayer) { 
-    this.Data = dataLayer;
+
+function ComputeCore(computeState) { 
+    this.Data = computeState;
+    this.CurrentDimension = null; 
 }
 
-ComputeCore.prototype.sum = function(range) {return _.reduce(range,function(memo,num) { memo + num },0);}
-    
-ComputeCore.prototype.subtract = function(range) {return _.reduce(range, function(memo, num) { memo - num },0);}
 
-ComputeCore.prototype.divide = function(range) {return _.reduce(range,function(memo,num) { memo - num } ,1 ); }
 
-ComputeCore.prototype.current = function(node) { return null; }
+ComputeCore.prototype.current = function(node) {
+    var query = this.CurrentDimension; 
+    query.Name = node;
+   
+    return this.Data.find(query)[0].Balance; 
+ }
     
-ComputeCore.prototype.range = function(node,type) { return null;}
+
+ComputeCore.prototype.getVal = function(val)  { 
+   
+    if(isNaN(val)) {
+        return this.current(val); 
+     }else {
+         return val; 
+     }
+}
+
+ComputeCore.prototype.range = function(node) { return this.Data.find(range);}
 
 ComputeCore.prototype.filter  = function(range,fn) {return _.Where(range,fn);};
 
-//The node is an important construct.All relationships in real world
-//are between accounts i.e. Asset = Long Term Asset + Short Term
-//Asset.But within the system at computation there are 6 dimensions
-//that uniquely define an account.
-function Node(name,time,model,scenario,company,valueType) {
-    this.name = name;
-    this.time = time;
-    this.model = model;
-    this.scenario = scenario;
-    this.company = company;
-}
+
+ComputeCore.prototype.setCurrent = function(dimension) { 
+    this.CurrentDimension = dimension; 
+    }
+
+ComputeCore.prototype.sum = function(range) {var that = this;  return _.reduce(Array.prototype.slice.call(arguments),function(memo,num) { return memo + that.getVal(num) },0);}
+
+ComputeCore.prototype.subtract = function(range) {var that = this;  return _.reduce(Array.prototype.slice.call(arguments),function(memo,num) { return  that.getVal(num) - memo },0);}
+    
+
+ComputeCore.prototype.multiply = function(range) {var that = this;  return _.reduce(Array.prototype.slice.call(arguments),function(memo,num) { return memo * that.getVal(num) },1);}
+
+ComputeCore.prototype.divide = function(range) {var that = this;  return _.reduce(Array.prototype.slice.call(arguments),function(memo,num) { return  that.getVal(num) / memo },1);}
+
+
+
+//exports.ComputeEngine = ComputeCore; 
+
 //The load process works are follows. There will be two parallel
-//thread of computation that will works . One which will find out all
-//the entries to close. The other will be the core computation. The
-//core computation will works simply loading the base (from company
-//template) as parts of tree, the rest will be done using a graph
-//caluclation.Thus in Core Computation object what will be send will a
+//thread of computation that will works . One which will find ou≈Xt all
+//the entries to close. The other will be the core computation. //caluclation.Thus in Core Computation object what will be send will a
 //tree. It is usefull to think in terms of mutually recursive
 //relations between two core process where the shared state is the
 //data object . Where Core Compute seeks from and while Core Model
